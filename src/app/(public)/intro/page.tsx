233586a4e93576a24413a295b638385af97de084
sohyerim-dev <fnmnn1@gmail.com>
2026-01-22 15:07:36 +0900

    쿠키 사용 방문이력 저장, 조회 및 페이지 이동 버튼 추가

---
+++ src/app/(public)/intro/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+export default function IntroPage() {
+    const router = useRouter()
+
+  const markVisited = () => {
+    localStorage.setItem('hasVisited', 'true')
+  }
+
+  const goToTest = () => {
+    markVisited()
+    router.push('/recommend')
+  }
+
+  const goToMain = () => {
+    markVisited()
+    router.push('/')
+  }
+
+  return (
+    <div>
+      <h1>인트로 페이지</h1>
+      <button onClick={goToTest}>테스트 하러 가기</button>
+      <button onClick={goToMain}>메인으로 가기</button>
+    </div>
+  )
+
+}
