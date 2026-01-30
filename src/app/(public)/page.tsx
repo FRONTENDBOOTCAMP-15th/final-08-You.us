d781627a4b01376c351f1a91a418f0afb11f8e0a
dltmdrb667 <dltmdrb667@naver.com>
2026-01-30 16:35:40 +0900

    feat: 인트로 메인페이지 로직추가

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,16 @@
 import Footer from '@/components/common/Footer/Footer'
 import Header from '@/components/common/Header/Header'
 import Main from '@/components/pages/main/Main'
-export default function ThemeTestPage() {
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+export default async function ThemeTestPage() {
+  const cookieStore = await cookies()
+  const hasVisited = cookieStore.get('hasVisited')
+
+  if (!hasVisited) {
+    redirect('/intro')
+  }
+
   return (
     <div className="mx-auto max-w-[1500px] bg-gray-50">
       <Header />
