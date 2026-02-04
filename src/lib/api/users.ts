0e436e759a681da8dc4754bb02217dd358e130da
sohyerim-dev <fnmnn1@gmail.com>
2026-02-04 15:42:23 +0900

    회원가입 기능 완성

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,9 +1,8 @@
 'use server';
 
+import fetchClient from '@/lib/api/fetchClient';
 import { ErrorRes, UserInfoRes } from '@/types/api.types';
-
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
+import { redirect } from 'next/navigation';
 
 type UserActionState = UserInfoRes | ErrorRes | null;
 
@@ -15,11 +14,10 @@ type UserActionState = UserInfoRes | ErrorRes | null;
  * @description
  * 첨부파일(프로필 이미지)이 있으면 파일 업로드 후 받은 파일경로를 회원 정보에 추가해서 회원가입 API를 호출
  */
-export async function createUser(
-  state: UserActionState,
+export async function signup(
+  _state: UserActionState,
   formData: FormData,
 ): Promise<UserActionState> {
-  let res: Response;
   let data: UserInfoRes | ErrorRes;
 
   try {
@@ -30,27 +28,29 @@ export async function createUser(
       name: formData.get('name'),
       email: formData.get('email'),
       password: formData.get('password'),
-      // ...(image ? { image } : {}),
+      address: {
+        postalCode: formData.get('postalCode'),
+        streetAddress: `${formData.get('address')}, ${formData.get('detail-address')}`,
+      },
+      phone: formData.get('phone'),
     };
 
     // 회원가입 API 호출
-    res = await fetch(`${API_URL}/users`, {
+    data = await fetchClient<UserInfoRes>('/users', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Client-Id': CLIENT_ID,
-      },
       body: JSON.stringify(body),
     });
-
-    data = await res.json();
   } catch (error) {
     // 네트워크 오류 처리
     console.error(error);
     return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
   }
 
-  return data;
+  if (data.ok === 1) {
+    redirect('/login');
+  } else {
+    return data; // 실패 시에만 리턴
+  }
 }
 
 /**
@@ -62,7 +62,7 @@ export async function createUser(
  * 이메일/비밀번호로 로그인 API 호출
  */
 export async function login(
-  state: UserActionState,
+  _state: UserActionState,
   formData: FormData,
 ): Promise<UserActionState> {
   const email = formData.get('email');
@@ -73,27 +73,17 @@ export async function login(
   };
   console.log('body', body);
   console.log('formData', formData);
-  let res: Response;
-  let data: UserInfoRes | ErrorRes;
 
   try {
-    // 로그인 API 호출
-    res = await fetch(`${API_URL}/users/login`, {
+    const data = await fetchClient<UserInfoRes>('/users/login', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Client-Id': CLIENT_ID,
-      },
       body: JSON.stringify(body),
     });
-    console.log('res', res);
-    data = await res.json();
+
     console.log('data', data);
+    return data;
   } catch (error) {
-    // 네트워크 오류 처리
     console.error(error);
     return { ok: 0, message: '일시적인 네트워크 문제가 발생했습니다.' };
   }
-
-  return data;
 }
