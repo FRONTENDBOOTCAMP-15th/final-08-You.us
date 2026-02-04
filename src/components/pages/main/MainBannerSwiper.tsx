cf80ff7026b77f75788efea7e2a6b5d951a324dd
sohyerim-dev <fnmnn1@gmail.com>
2026-02-04 11:35:38 +0900

    스와이프 이미지 추가

--- src/components/pages/main/MainBannerSwiper.tsx
+++ src/components/pages/main/MainBannerSwiper.tsx
@@ -7,6 +7,8 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 import styles from './Main-swiper.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function MainBannerSwiper() {
   return (
@@ -26,40 +28,41 @@ export default function MainBannerSwiper() {
         speed={500}
       >
         <SwiperSlide>
-          <div className="flex h-full w-full items-center justify-center bg-linear-to-r from-blue-500 to-purple-600">
-            <div className="text-center text-white">
-              <h2 className="text- mb-4 font-bold lg:text-5xl">
-                신년 특별 이벤트
-              </h2>
-              <p className="mb-6 text-lg lg:text-2xl">전 상품 최대 30% 할인</p>
-              <button className="rounded-full bg-red-500 px-8 py-3 font-bold text-white hover:bg-red-600">
-                지금 확인 →
-              </button>
-            </div>
+          <div className="flex h-full w-full items-center justify-center bg-gray-900 bg-linear-to-r">
+            <Link href="/recommend">
+              <Image
+                width={1024}
+                height={500}
+                className="mx-auto hidden lg:block"
+                alt=""
+                src="/images/banners/swiper-image-1-lg.svg"
+              />
+              <Image
+                width={360}
+                height={280}
+                className="mx-auto block lg:hidden"
+                alt=""
+                src="/images/banners/swiper-image-1-sm.svg"
+              />
+            </Link>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="flex h-full w-full items-center justify-center bg-linear-to-r from-amber-500 to-orange-600">
-            <div className="text-center text-white">
-              <h2 className="mb-4 text-3xl font-bold lg:text-5xl">특가 세일</h2>
-              <p className="mb-6 text-lg lg:text-2xl">오늘만 특별한 가격</p>
-              <button className="rounded-full bg-red-500 px-8 py-3 font-bold text-white hover:bg-red-600">
-                지금 확인 →
-              </button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex h-full w-full items-center justify-center bg-linear-to-r from-green-500 to-teal-600">
-            <div className="text-center text-white">
-              <h2 className="mb-4 text-sm font-bold lg:text-5xl">
-                신상품 출시
-              </h2>
-              <p className="mb-6 text-lg lg:text-2xl">최신 트렌드 상품</p>
-              <button className="rounded-full bg-red-500 px-8 py-3 font-bold text-white hover:bg-red-600">
-                지금 확인 →
-              </button>
-            </div>
+          <div className="flex h-full w-full items-center justify-center bg-gray-900 bg-linear-to-r">
+            <Image
+              width={1024}
+              height={500}
+              className="mx-auto hidden lg:block"
+              alt=""
+              src="/images/banners/swiper-image-2-lg.svg"
+            />
+            <Image
+              width={360}
+              height={280}
+              className="mx-auto block lg:hidden"
+              alt=""
+              src="/images/banners/swiper-image-2-sm.svg"
+            />
           </div>
         </SwiperSlide>
       </Swiper>
