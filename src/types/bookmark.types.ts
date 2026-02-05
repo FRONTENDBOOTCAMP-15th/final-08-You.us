9553b864d455ecaebafa79e5f5b21a8d77d4adcd
dltmdrb667 <dltmdrb667@naver.com>
2026-02-06 01:47:38 +0900

    feat:북마크 타입

---
+++ src/types/bookmark.types.ts
@@ -0,0 +1,34 @@
+import { ProductImage } from './product.types';
+
+export interface BookmarkItem {
+  _id: number;
+  createdAt: string;
+  user_id: number;
+  product: {
+    _id: number;
+    name: string;
+    price: number;
+    quantity: number;
+    buyQuantity: number;
+    mainImages: ProductImage[];
+    extra: {
+      category: string[];
+      sort: number;
+      tags: string[];
+      options: Record<string, unknown>;
+    };
+    seller: {
+      _id: number;
+      name: string;
+      email: string;
+      image: string;
+    };
+    bookmarks: number;
+    likes: number;
+  };
+}
+
+export interface BookmarkResponse {
+  ok: number;
+  item: BookmarkItem;
+}
